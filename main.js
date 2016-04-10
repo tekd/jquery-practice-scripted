727085dcd6c2c2e654681c16e5681669203a81b9
Denny Tek <tek.denny@gmail.com>
2016-04-10 14:51:19 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    alert("HELLO");
+    $("h1").text("jquery is loaded"); 
+    $(".instructions").append("<p>Use this <strong>boilerplate</strong> for your projects.</p>");
+})
